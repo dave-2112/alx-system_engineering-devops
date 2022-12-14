db536396993dd4bcfd9ec3fc0b83245dd02efa4b
dave-2112 <davemesfin16@gmail.com>
2022-12-14 17:28:07 +0300

    ssh

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++
@@ -1,6 +0,0 @@
-# modify ssh config file
-exec {'/etc/ssh/ssh_config':
-path     => '/bin',
-command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-provider => 'shell',
-}
